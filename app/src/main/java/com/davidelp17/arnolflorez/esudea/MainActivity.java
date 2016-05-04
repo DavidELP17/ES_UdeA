ce8ec2724eda98425d2d29bbebfc87b9f84f6985
DavidELP17 <davidelp94@gmail.com>
2016-05-04 03:17:50 -0500

    By: DavidELP17 Tabs con Recycler View y Fragment Fragment Infromacion Detallada Backstack 1 Navigation Drawer con Activities 1

--- app/src/main/java/com/davidelp17/arnolflorez/esudea/MainActivity.java
+++ app/src/main/java/com/davidelp17/arnolflorez/esudea/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
@@ -20,8 +21,11 @@ import com.davidelp17.arnolflorez.esudea.utils.Utils;
 
 public class MainActivity extends AppCompatActivity
 {
-    public static final String TAG = "MainActivity";
+    public static final String TAGMainActivity = "MainActivity";
     public static final String IMAGE_CACHE_DIR = "thumbs";
+
+    private int mImageThumbSize;
+    private int mImageThumbSpacing;
 
     private NavigationView navView;
     private DrawerLayout mDrawerLayout;
@@ -73,56 +77,67 @@ public class MainActivity extends AppCompatActivity
                         boolean fragmentTransaction = false;
                         Fragment fragment = null;
 
+                        int opc = 0;
+
                         switch (menuItem.getItemId()) {
                             case R.id.nav_home:
                                 fragment = new HomeFragment();
                                 fragmentName1 = HomeFragment.TAGHome;
                                 fragmentTransaction = true;
+                                opc = 1;
                                 break;
                             case R.id.nav_university:
-                                fragment = new UniversityFragment();
-                                fragmentName1 = UniversityFragment.TAGUniversity;
-                                fragmentTransaction = true;
+                                Intent MainActivity1 = new Intent(getApplicationContext(), MainActivityTabs.class);
+                                startActivity(MainActivity1);
+                                opc = 2;
                                 break;
                             case R.id.nav_perfil:
                                 fragment = new ProfileFragment();
                                 fragmentName1 = ProfileFragment.TAGProfile;
                                 fragmentTransaction = true;
+                                opc = 3;
                                 break;
                             case R.id.nav_calendar:
                                 fragment = new CalendarFragment();
                                 fragmentName1 = CalendarFragment.TAGCalendar;
                                 fragmentTransaction = true;
+                                opc = 4;
                                 break;
                             case R.id.nav_horario:
                                 fragment = new HorarioFragment();
                                 fragmentName1 = HorarioFragment.TAGHorario;
                                 fragmentTransaction = true;
+                                opc = 5;
                                 break;
                             case R.id.nav_grupos:
                                 fragment = new GruposFragment();
                                 fragmentName1 = GruposFragment.TAGGrupos;
                                 fragmentTransaction = true;
+                                opc = 6;
                                 break;
                             case R.id.nav_mapas:
                                 fragment = new MapsFragment();
                                 fragmentName1 = MapsFragment.TAGMaps;
                                 fragmentTransaction = true;
+                                opc = 7;
                                 break;
                             case R.id.nav_galeria:
                                 fragment = new PhotoGalleryFragment();
                                 fragmentName1 = PhotoGalleryFragment.TAGPhotoGallery;
                                 fragmentTransaction = true;
+                                opc = 8;
                                 break;
                             case R.id.nav_sitioweb:
                                 fragment = new WebsiteFragment();
                                 fragmentName1 = WebsiteFragment.TAGWebsite;
                                 fragmentTransaction = true;
+                                opc = 9;
                                 break;
                             case R.id.nav_info:
                                 fragment = new InfoFragment();
                                 fragmentName1 = InfoFragment.TAGInfo;
                                 fragmentTransaction = true;
+                                opc = 10;
                                 break;
                             case R.id.nav_exit:
                                 finish();
@@ -132,21 +147,59 @@ public class MainActivity extends AppCompatActivity
                                 startActivity(intent);
                         }
 
-                        if(fragmentTransaction) {
-                            getSupportFragmentManager().beginTransaction()
-                                    .replace(R.id.content_frame, fragment,fragmentName1)
-                                    .addToBackStack(fragmentName1)
-                                    .commit();
+                        //fragmentTransaction = noabrirdoble(fragmentTransaction, opc);
+                        replaceFragment(fragment, fragmentTransaction);
 
-                            menuItem.setChecked(true);
-                            getSupportActionBar().setTitle(menuItem.getTitle());
-                        }
+                        menuItem.setChecked(true);
+                        getSupportActionBar().setTitle(menuItem.getTitle());
 
                         mDrawerLayout.closeDrawers();
 
                         return true;
                     }
                 });
+
+        navView.getMenu().getItem(0).setChecked(true);
+    }
+
+    /*
+    public boolean noabrirdoble(boolean fragmentTransaction, int menuItem){
+
+        ProfileFragment testProfile = (ProfileFragment) getSupportFragmentManager().findFragmentByTag(ProfileFragment.TAGProfile);
+        MatchesFragment testMatch = (MatchesFragment) getSupportFragmentManager().findFragmentByTag(MatchesFragment.TAGMatches);
+        StandingFragment testStanding = (StandingFragment) getSupportFragmentManager().findFragmentByTag(StandingFragment.TAGStandings);
+
+        if ((testProfile != null && testProfile.isVisible()) && menuItem == 1) {
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+            fragmentTransaction = false;
+        }
+        if ((testMatch != null && testMatch.isVisible()) && menuItem == 2) {
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+            fragmentTransaction = false;
+        }
+        if ((testStanding != null && testStanding.isVisible()) && menuItem == 3) {
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+            fragmentTransaction = false;
+        }
+
+        return fragmentTransaction;
+    }
+    */
+
+    private void replaceFragment(Fragment fragment, boolean fragmentTransaction){
+        String backStateName = fragment.getClass().getName();
+
+        FragmentManager manager = getSupportFragmentManager();
+        boolean fragmentPopped = manager.popBackStackImmediate(backStateName, 0);
+
+        if (!fragmentPopped){ //fragment not in back stack, create it.
+            if (fragmentTransaction) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.content_frame, fragment, fragmentName1)
+                        .addToBackStack(fragmentName1)
+                        .commit();
+            }
+        }
     }
 
     public void setFragment(String fragmentName, int container, boolean backStack) {
